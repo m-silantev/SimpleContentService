20da1ea8ff3801ecbc969f55557d8b1ab87f831f
m.silantev <indigoleague23@gmail.com>
2024-02-29 11:05:11 +0300

    create application.properties if it doesn't exist

--- src/main/java/tech/silantev/simple/cdn/Main.java
+++ src/main/java/tech/silantev/simple/cdn/Main.java
@@ -1,6 +1,9 @@
 package tech.silantev.simple.cdn;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Properties;
 
 public class Main {
@@ -11,7 +14,16 @@ public class Main {
 
     private static Properties loadProperties() {
         Properties properties = new Properties();
-        try (FileInputStream fis = new FileInputStream("application.properties")) {
+        File file = new File("application.properties");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                Files.write(file.toPath(), "pauseMs=500\n".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try (FileInputStream fis = new FileInputStream(file)) {
             properties.load(fis);
         } catch (Exception e) {
             e.printStackTrace();
