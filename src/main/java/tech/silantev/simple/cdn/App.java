71dc484d1c2bee6fbfd788412d13406fea980af8
Shiki <indigoleague23@gmail.com>
2024-03-04 01:16:14 +0300

    java version WIP

--- src/main/java/tech/silantev/simple/cdn/App.java
+++ src/main/java/tech/silantev/simple/cdn/App.java
@@ -1,10 +1,12 @@
 package tech.silantev.simple.cdn;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
 import tech.silantev.simple.cdn.properties.Properties;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.file.Paths;
 
 public class App {
 
@@ -15,7 +17,8 @@ public class App {
     }
 
     public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(properties.getPort()), 0);
+        HttpServer server = SimpleFileServer.createFileServer(new InetSocketAddress(properties.getPort()),
+                Paths.get(""), SimpleFileServer.OutputLevel.INFO);
         server.createContext("/", new Handler(properties));
         server.setExecutor(null);
         server.start();
